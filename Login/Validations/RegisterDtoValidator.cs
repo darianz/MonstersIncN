abcc73495d58129864d335ebf044ec5fc611da82
Ron_Stoleru <ron.s@shoesonline.co.il>
2023-01-16 05:05:38 +0200

    RegisterDtoValidator using FluentValidation

--- Login/Validations/RegisterDtoValidator.cs
+++ Login/Validations/RegisterDtoValidator.cs
@@ -9,7 +9,8 @@ namespace Login.Validations
         {
             RuleFor(x => x.FirstName).NotEmpty().WithMessage("FirstName is required");
             RuleFor(x => x.LastName).NotEmpty().WithMessage("LastName is required");
-            RuleFor(x => x.Password).NotEmpty().MinimumLength(8).WithMessage("Password must be at least 8 characters long");
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(8).WithMessage("Password must be at least 8 characters long")
+            .Matches("^(?=.[A-Z])(?=.[0-9])(?=.*[^a-zA-Z0-9]).{8,}$").WithMessage("Password must contain at least one capital letter, one digit, and one non-alphanumeric character.");
             RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("PhoneNumber is required");
             RuleFor(x => x.TentaclesNumber).NotEmpty().WithMessage("TentaclesNumber is required");
         }
